362ad4f5da90a0d3a6807f7107fdcc886de50c00
adityashukla2799 <adityashukla2799@gmail.com>
2017-09-08 19:07:40 -0300

    Hmmmmmm

--- Aditya.java
+++ Aditya.java
@@ -5,8 +5,6 @@ public class Aditya {
 public static void main(String[] args){
 int a,b,c;
 System.out.println("Enter any 2 integers : ");
-java.util.Scanner.nextLong(int radix);
-java.util.Scanner.next(int radix);
 c=a+b;
 System.out.println("The sum is : "+c);
 }
