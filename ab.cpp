34a31dc3973ee6c819afd2d40ab3a0c44d7d668a
adityashukla2799 <adityashukla2799@gmail.com>
2017-09-08 16:18:25 -0300

    Java added.

---
+++ ab.cpp
@@ -0,0 +1,16 @@
+//
+// Created by aditya on 9/8/17.
+//
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    clrscr();
+    int a,b,c;
+    printf("Enter 2 integers : ");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    c=a+b;
+    printf("The sum is : %d",c);
+    getch();
+}
---
+++ cmake-build-debug/addd.cpp
@@ -0,0 +1,4 @@
+//
+// Created by aditya on 9/8/17.
+//
+
